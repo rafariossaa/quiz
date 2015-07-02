8746d09afeb8ec8de6655a4371e49c0f8987d664
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2015-07-02 22:49:42 +0200

    quitado sequelize.success

--- models/models.js
+++ models/models.js
@@ -33,7 +33,7 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
 
 // sequelize.sync() crea e inicializa talba de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // success(..) ejecuta el manjejador una vez creada la tabla
   Quiz.count().success(function (count) {
     if (count === 0) {
