c2b35396372a91b8e56d7e74393d41934175c27c
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2015-07-07 20:02:21 +0200

    Modulo09 - Quiz16 - autenticacion y sesion

--- models/models.js
+++ models/models.js
@@ -28,9 +28,18 @@ var sequelize = new Sequelize(DB_name, user, pwd,
                             });
 
 //Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
+exports.Comment = Comment; 
 
 // sequelize.sync() crea e inicializa talba de preguntas en DB
 sequelize.sync().then(function() {
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,6 +15,12 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
+// Definicion de rutas de sesion
+router.get('/login',	sessionController.new);  // formulario login
+router.post('/login',   sessionController.create); // crear sesion
+router.get('/logout',   sessionController.destroy); // destruir sesion
+
+// Definicion de rutas de /quizes
 router.get('/quizes',				quizController.index);
 router.get('/quizes/:quizId(\\d+)',		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
@@ -20,6 +29,10 @@ router.post('/quizes/create',			quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',		quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+
 
 router.get('/author', function(req, res) {
   res.render('author');
